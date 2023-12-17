44426d3112b0c753a8865ff620b5225def6d439f
stakhovyak <stakhovyak@DESKTOP-QJUUHR6>
2023-12-17 11:12:10 -0800

    Add project files.

---
+++ vladlab1var11/Form1.h
@@ -0,0 +1,252 @@
+﻿#pragma once
+
+namespace CppCLRWinFormsProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(27, 90);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(259, 20);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &Form1::textBox1_KeyPress);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(26, 176);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(260, 20);
+			this->textBox2->TabIndex = 1;
+			this->textBox2->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &Form1::textBox2_KeyPress);
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(0, 0);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 12;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(30, 72);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(157, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Введите одномерный массив";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(28, 157);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(142, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Преобразованный массив";
+			// 
+			// label3
+			// 
+			this->label3->Location = System::Drawing::Point(0, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(100, 23);
+			this->label3->TabIndex = 11;
+			// 
+			// label4
+			// 
+			this->label4->Location = System::Drawing::Point(0, 0);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(100, 23);
+			this->label4->TabIndex = 10;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(292, 87);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(119, 23);
+			this->button1->TabIndex = 7;
+			this->button1->Text = L"Преобразовать";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(292, 173);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(119, 23);
+			this->button2->TabIndex = 8;
+			this->button2->Text = L"Сложить";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &Form1::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(472, 370);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(119, 23);
+			this->button3->TabIndex = 9;
+			this->button3->Text = L"Выход";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &Form1::button3_Click);
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(603, 405);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->MaximizeBox = false;
+			this->Name = L"Form1";
+			this->Text = L"none";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+
+		private:
+			static array<char>^ s = gcnew array<char>(200);
+			static array<char>^ l = gcnew array<char>(200);
+			static array<int>^ A = gcnew array<int>(50);
+			static array<int>^ Q = gcnew array<int>(50);
+			static int dim = 1;
+			static int f = 0;
+			static int e1 = 0;
+
+			// вынести все нули в начало
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		String^ input = textBox1->Text;
+		array<String^>^ elements = input->Split(' ');
+		array<String^>^ result = gcnew array<String^>(elements->Length);
+		int index = 0;
+
+		for each (String ^ element in elements) {
+			if (element == "0") {
+				for (int i = index; i > 0; i--) {
+					result[i] = result[i - 1]; // Shift elements to the right
+				}
+				result[0] = element; // Insert the zero at the beginning of the array
+			}
+			else {
+				result[index] = element; // Assign non-zero elements to the current index
+			}
+			index++;
+		}
+
+		textBox2->Text = String::Join(" ", result); // Set the transformed array as the text of textBox2
+	}
+		   private: System::Void textBox1_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+
+			   if (((e->KeyChar >= '0') && (e->KeyChar <= '9')) || (e->KeyChar == 8) || (e->KeyChar == ' '))
+				   //цифра
+				   return;
+			   //остальные символы запрещены
+			   e->Handled = true;
+		   }
+private: System::Void textBox2_KeyPress(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e) {
+
+	if (((e->KeyChar >= '0') && (e->KeyChar <= '9')) || (e->KeyChar == 8) || (e->KeyChar == ' ') ||
+		(e->KeyChar == '-')) return;
+	//Остальные символы запрещены
+	e->Handled = true;
+}
+
+	   // суммирование элементов
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	String^ input = textBox2->Text;
+	array<String^>^ elements = input->Split(' ');
+	int sum = 0;
+
+	for each (String ^ element in elements) {
+		sum += System::Convert::ToInt32(element);
+	}
+
+	MessageBox::Show("Sum of the entered elements: " + sum, "Sum Summary", MessageBoxButtons::OK, MessageBoxIcon::Information);
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+
+	Close();
+}
+private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+
+}
